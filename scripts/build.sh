ee8204005ae14060dd927077f4c2804a6fc3b3c5
kevincar <kevincarrolldavis@gmail.com>
2018-05-19 21:52:07 -0500

    Made it executable

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,15 @@ function fail_and_exit() {
 	exit 1
 }
 
+echo "Creating build directory"
 mkdir build || fail_and_exit "Failed to make directory"
+
+echo "Moving into build directory"
 cd ./build || fail_and_exit "Failed to cd into build"
+
+echo "Generating build environment..."
 cmake .. || fail_and_exit "Failed to generate the build environment"
+
+echo "Making..."
 make || fail_and_exit "Failed to make"
 #make install || fail_and_exit "Failed to install"

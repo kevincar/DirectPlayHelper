d74c723b77ae97571edda1a7e9bb5919ff064478
kevincar <kevincarrolldavis@gmail.com>
2018-05-19 23:10:47 -0500

    unzip and build gtest for travis-ci

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env sh
 
 brew install g3log
-brew install gtest
+
+#brew install gtest
+cd ./tests
+unzip googletest-release-1.8.0.zip -d gtest
+cd ./gtest/googletest-release-1.8.0/
+mkdir build
+cd ./build
+cmake ..
+make

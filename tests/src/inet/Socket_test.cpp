b39a168dff5b50530aba4ddd7060e4c576aa9f19
kevincar <kevincarrolldavis@gmail.com>
2018-05-24 23:51:15 -0400

    Add both failing and success tests

--- tests/src/inet/Socket_test.cpp
+++ tests/src/inet/Socket_test.cpp
@@ -1,7 +1,17 @@
+#include <sys/socket.h>
 #include "inet/Socket.hpp"
 #include "gtest/gtest.h"
 
 TEST(SocketTest, constructor)
 {
+	EXPECT_NO_THROW({
+			inet::Socket socket(AF_INET, SOCK_STREAM, 0);
+			});
+}
 
+TEST(SocketTest, constructorFail)
+{
+	EXPECT_ANY_THROW({
+			inet::Socket socket(999, 999, 999);
+			});
 }

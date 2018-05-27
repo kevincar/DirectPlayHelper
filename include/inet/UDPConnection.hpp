0a16b68f4e5d120691d6fdfc321103695f348803
kevincar <kevincarrolldavis@gmail.com>
2018-05-26 23:54:54 -0400

    Start UDPConnection interface

---
+++ include/inet/UDPConnection.hpp
@@ -0,0 +1,15 @@
+
+#ifndef INET_UDP_CONNECTION_HPP
+#define INET_UDP_CONNECTION_HPP
+
+#include "inet/IPConnection.hpp"
+
+namespace inet
+{
+	class UDPConnection : public IPConnection
+	{
+
+	};
+}
+
+#endif /* INET_UDP_CONNECTION_HPP */

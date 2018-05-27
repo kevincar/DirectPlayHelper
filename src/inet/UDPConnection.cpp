1f60887a1e017f8f270a35b9b098467664fe134b
kevincar <kevincarrolldavis@gmail.com>
2018-05-27 19:30:18 -0400

    Interface for send

--- src/inet/UDPConnection.cpp
+++ src/inet/UDPConnection.cpp
@@ -1,8 +1,19 @@
 
+#include <iostream>
 #include "sys/socket.h"
 #include "inet/UDPConnection.hpp"
 
 namespace inet
 {
 	UDPConnection::UDPConnection() : IPConnection(SOCK_DGRAM, 0) {}
+
+	bool UDPConnection::send(void* data)
+	{
+		std::cout << "Not yet implemented";
+		if(data == nullptr)
+		{
+			return false;
+		}
+		return true;
+	}
 }

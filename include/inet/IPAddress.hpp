df2a90e87879bb7b2f6e3a3880979cfee3b18011
kevincar <kevincarrolldavis@gmail.com>
2018-05-28 11:47:12 -0400

    Added IPAddress

---
+++ include/inet/IPAddress.hpp
@@ -0,0 +1,23 @@
+
+#ifndef INET_IP_ADDRESS_HPP
+#define INET_IP_ADDRESS_HPP
+
+#include <string>
+#include <netinet/in.h>
+#include <mutex>
+
+namespace inet
+{
+	class IPAddress
+	{
+		public:
+			IPAddress(std::string IPAddressString);
+
+			std::string getAddress(void) const;
+		private:
+			sockaddr_in addr {};
+			std::mutex addr_mutex;
+	};
+}
+
+#endif /* INET_IP_ADDRESS_HPP */

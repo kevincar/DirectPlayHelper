44603d84df821830e5e299059746bfc727ae0054
kevincar <kevincarrolldavis@gmail.com>
2018-05-29 20:02:27 -0400

    Added thread-safe checks

--- include/inet/IPConnection.hpp
+++ include/inet/IPConnection.hpp
@@ -19,7 +19,9 @@ namespace inet
 			virtual bool send(void* data) const = 0;
 
 		private:
-			std::mutex socket_mutex;
+			mutable std::mutex socket_mutex;
+			mutable std::mutex srcAddr_mutex;
+			mutable std::mutex destAddr_mutex;
 			std::shared_ptr<Socket> socket;
 			std::unique_ptr<ServiceAddress> srcAddress;
 			std::unique_ptr<ServiceAddress> destAddress;
--- src/inet/IPConnection.cpp
+++ src/inet/IPConnection.cpp
@@ -13,15 +13,18 @@ namespace inet
 
 	std::string const IPConnection::getAddressString(void) const
 	{
+		std::lock_guard<std::mutex> lock {this->srcAddr_mutex};
 		return this->srcAddress->getAddressString();
 	}
 
 	void IPConnection::setAddress(std::string const& address)
 	{
 		// Set the address
+		std::lock_guard<std::mutex> srcAddr_lock {this->srcAddr_mutex};
 		this->srcAddress = std::make_unique<ServiceAddress>(address);
 		
 		// Bind
+		std::lock_guard<std::mutex> sock_lock {this->socket_mutex};
 		this->srcAddress->bind(this->socket);
 	}
 }

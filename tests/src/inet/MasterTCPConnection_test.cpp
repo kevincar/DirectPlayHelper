89fb29c35f7cf0ecffd5fa1823144ced82e3454a
kevincar <kevincarrolldavis@gmail.com>
2018-06-04 22:33:33 -0400

    Begin testing for listenForIncomingConnections

--- tests/src/inet/MasterTCPConnection_test.cpp
+++ tests/src/inet/MasterTCPConnection_test.cpp
@@ -1,4 +1,4 @@
-
+#include <functional>
 #include "inet/MasterTCPConnection.hpp"
 #include "gtest/gtest.h"
 
@@ -7,4 +7,20 @@ TEST(MasterTCPConnectionTest, constructor)
 	ASSERT_NO_THROW({
 			inet::MasterTCPConnection mtcpc;
 			});
+}
+
+TEST(MasterTCPConnectionTest, listenForIncomingConnections)
+{
+	// Define the accept Handler
+	inet::MasterTCPConnection::newConnectionAcceptHandlerFunc ncah = [](std::shared_ptr<inet::TCPConnection>& nc) -> bool {if(nc != nullptr) return true; else return false;};
+	
+	std::shared_ptr<inet::TCPConnection> sptcp = std::make_shared<inet::TCPConnection>();
+	// Define the process Handler
+	inet::MasterTCPConnection::connectionProcessHandlerFunc cph = [](std::shared_ptr<inet::TCPConnection>& nc){if(nc){}};
+
+	// Create a new MasterTCPConnection
+	inet::MasterTCPConnection master;
+
+	// Begin listening for incomming connections
+	//master.listenForIncomingConnections(ncah, cph);
 }

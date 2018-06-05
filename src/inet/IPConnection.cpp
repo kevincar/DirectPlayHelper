8da36c12f307542b3af05f4611e874a12c7c5398
kevincar <kevincarrolldavis@gmail.com>
2018-06-04 22:32:39 -0400

    Implemented isDataReady

--- src/inet/IPConnection.cpp
+++ src/inet/IPConnection.cpp
@@ -1,4 +1,7 @@
-#include "sys/socket.h"
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <cmath>
 #include "inet/Socket.hpp"
 #include "inet/ServiceAddress.hpp"
 #include "inet/IPConnection.hpp"
@@ -45,5 +48,30 @@ namespace inet
 	{
 		std::lock_guard<std::mutex> lock {this->srcAddr_mutex};
 		this->srcAddress->listen(this->socket);
+	}
+
+	bool IPConnection::isDataReady(double timeout) const
+	{
+		fd_set fs;
+		FD_ZERO(&fs);
+
+		std::lock_guard<std::mutex> lock {this->socket_mutex};
+		FD_SET(*this->socket.get(), &fs);
+
+		timeout = std::abs(timeout);
+		double seconds = floor(timeout);
+		double remainder = timeout - seconds;
+		double microseconds = floor(remainder * 1e6);
+		struct timeval tv;
+		tv.tv_sec = static_cast<int>(seconds);
+		tv.tv_usec = static_cast<int>(microseconds);
+
+		int result = ::select(*this->socket.get()+1, &fs, nullptr, nullptr, &tv);
+		if(result == -1)
+		{
+			throw std::string("IPConnection::isDataReady failed with errno: ") + std::to_string(errno);
+		}
+
+		return FD_ISSET(*this->socket.get(), &fs);
 	}
 }

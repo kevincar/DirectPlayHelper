59d8b671173e2911aac7be1ad8400e636bc3f163
kevincar <kevincarrolldavis@gmail.com>
2018-07-21 08:31:27 -0400

    Comments until the design is more stable

--- tests/src/inet/MasterConnection_test.cpp
+++ tests/src/inet/MasterConnection_test.cpp
@@ -13,143 +13,143 @@ TEST(MasterConnectionTest, constructor)
 			});
 }
 
-TEST(MasterConnectionTest, acceptConnection)
-{
-	inet::MasterConnection mtcp;
-	std::shared_ptr<inet::TCPConnection> testConn = std::make_shared<inet::TCPConnection>();
+//TEST(MasterConnectionTest, acceptConnection)
+//{
+	//inet::MasterConnection mtcp;
+	//std::shared_ptr<inet::TCPConnection> testConn = std::make_shared<inet::TCPConnection>();
 	
-	ASSERT_EQ(mtcp.getNumConnections(), 0);
-	mtcp.acceptConnection(testConn);
-	ASSERT_EQ(mtcp.getNumConnections(), 1);
-}
+	//ASSERT_EQ(mtcp.getNumConnections(), 0);
+	//mtcp.acceptConnection(testConn);
+	//ASSERT_EQ(mtcp.getNumConnections(), 1);
+//}
 
-TEST(MasterConnectionTest, listenForIncomingConnections)
-{
-	// Define the accept Handler
-	inet::MasterConnection::newConnectionAcceptHandlerFunc ncah = [](std::shared_ptr<inet::TCPConnection>& nc) -> bool {
-		std::cout << "new connection!" << std::endl;
-		if(nc) {}
-		return true;
-	};
+//TEST(MasterConnectionTest, listenForIncomingConnections)
+//{
+	//// Define the accept Handler
+	//inet::MasterConnection::newConnectionAcceptHandlerFunc ncah = [](std::shared_ptr<inet::TCPConnection>& nc) -> bool {
+		//std::cout << "new connection!" << std::endl;
+		//if(nc) {}
+		//return true;
+	//};
 	
-	// Define the process Handler
-	bool recieved_payload = false;
-	inet::MasterConnection::connectionProcessHandlerFunc cph = [&](std::shared_ptr<inet::TCPConnection>& conn) -> bool {
-		std::cout << "Process conenction: " << static_cast<int>(*conn) << std::endl;
-		if(conn)
-		{
-			std::unique_ptr<char> payload {new char[255]{}};
-			int result = conn->recv(payload.get(), 255);
-			result = 0;
-			std::string payload_string {payload.get()};
-			recieved_payload = payload_string == "This is the data";
-		}
-		return false;
-	};
-
-	// Create a new MasterConnection
-	inet::MasterConnection master;
-
-	// Variables for multithreaded synchronization
-	std::condition_variable cv;
-	std::mutex cvm;
-	std::string step {};
-	std::string IPAddressString {};
-	// steps: Client Ready, Server Started, Client Connecting, Server Received
-	// Connection, Client Sending Data, Server Recieved Data, Client Done,
-	// Server Done
-
-	// Begin listening for incomming connections
-	//std::thread serverThread {[&](){
-		//// Wait for Client Ready
-		//std::unique_lock<std::mutex> lk{cvm};
-		//cv.wait(lk, [&]{return step == "Client Ready";});
-
-		//master.listenForIncomingConnections(ncah, cph);
-		//IPAddressString = master.getAddressString();
-		//step = "Server Started";
-
-		//lk.unlock();
-		//cv.notify_one();
-
-		//lk.lock();
-		//cv.wait(lk, [&]{return step == "Client Connecting";});
-
-		//// Wait 5 seconds for connection
-		//std::chrono::seconds timeout{5};
-		//std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-		//std::chrono::system_clock::time_point currentTime = std::chrono::system_clock::now();
-		//std::chrono::seconds elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - start);
-		//while(elapsedTime < timeout)
+	//// Define the process Handler
+	//bool recieved_payload = false;
+	//inet::MasterConnection::connectionProcessHandlerFunc cph = [&](std::shared_ptr<inet::TCPConnection>& conn) -> bool {
+		//std::cout << "Process conenction: " << static_cast<int>(*conn) << std::endl;
+		//if(conn)
 		//{
-			//if(master.getNumConnections() > 0) break;
-			//currentTime = std::chrono::system_clock::now();
-			//elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - start);
+			//std::unique_ptr<char> payload {new char[255]{}};
+			//int result = conn->recv(payload.get(), 255);
+			//result = 0;
+			//std::string payload_string {payload.get()};
+			//recieved_payload = payload_string == "This is the data";
 		//}
+		//return false;
+	//};
 
-		//ASSERT_EQ(master.getNumConnections(), 1);
-		//step = "Server Received Connection";
+	//// Create a new MasterConnection
+	//inet::MasterConnection master;
 
-		//lk.unlock();
-		//cv.notify_one();
+	//// Variables for multithreaded synchronization
+	//std::condition_variable cv;
+	//std::mutex cvm;
+	//std::string step {};
+	//std::string IPAddressString {};
+	//// steps: Client Ready, Server Started, Client Connecting, Server Received
+	//// Connection, Client Sending Data, Server Recieved Data, Client Done,
+	//// Server Done
 
-		//lk.lock();
-		//cv.wait(lk, [&]{return step == "Client Sending Data";});
+	//// Begin listening for incomming connections
+	////std::thread serverThread {[&](){
+		////// Wait for Client Ready
+		////std::unique_lock<std::mutex> lk{cvm};
+		////cv.wait(lk, [&]{return step == "Client Ready";});
 
-		//// Wait 5 seconds for recipt
-		//start = std::chrono::system_clock::now();
-		//currentTime = std::chrono::system_clock::now();
-		//elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - start);
-		//while(elapsedTime < timeout)
-		//{
-			//if(recieved_payload == true) break;
-			//currentTime = std::chrono::system_clock::now();
-			//elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - start);
-		//}
-		//ASSERT_EQ(recieved_payload, true);
-		//step = "Server Received Data";
+		////master.listenForIncomingConnections(ncah, cph);
+		////IPAddressString = master.getAddressString();
+		////step = "Server Started";
 
-		//lk.unlock();
-		//cv.notify_one();
-	//}};
+		////lk.unlock();
+		////cv.notify_one();
+
+		////lk.lock();
+		////cv.wait(lk, [&]{return step == "Client Connecting";});
+
+		////// Wait 5 seconds for connection
+		////std::chrono::seconds timeout{5};
+		////std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+		////std::chrono::system_clock::time_point currentTime = std::chrono::system_clock::now();
+		////std::chrono::seconds elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - start);
+		////while(elapsedTime < timeout)
+		////{
+			////if(master.getNumConnections() > 0) break;
+			////currentTime = std::chrono::system_clock::now();
+			////elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - start);
+		////}
+
+		////ASSERT_EQ(master.getNumConnections(), 1);
+		////step = "Server Received Connection";
+
+		////lk.unlock();
+		////cv.notify_one();
+
+		////lk.lock();
+		////cv.wait(lk, [&]{return step == "Client Sending Data";});
+
+		////// Wait 5 seconds for recipt
+		////start = std::chrono::system_clock::now();
+		////currentTime = std::chrono::system_clock::now();
+		////elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - start);
+		////while(elapsedTime < timeout)
+		////{
+			////if(recieved_payload == true) break;
+			////currentTime = std::chrono::system_clock::now();
+			////elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - start);
+		////}
+		////ASSERT_EQ(recieved_payload, true);
+		////step = "Server Received Data";
+
+		////lk.unlock();
+		////cv.notify_one();
+	////}};
 
 
-	//// attempt to connect
-	//std::thread clientThread {[&](){
-		//// Get Client Ready
-		//std::unique_lock<std::mutex> lk{cvm};
-		//inet::TCPConnection tcpc;
-		//step = "Client Ready";
+	////// attempt to connect
+	////std::thread clientThread {[&](){
+		////// Get Client Ready
+		////std::unique_lock<std::mutex> lk{cvm};
+		////inet::TCPConnection tcpc;
+		////step = "Client Ready";
 
-		//lk.unlock();
-		//cv.notify_one();
+		////lk.unlock();
+		////cv.notify_one();
 
-		//lk.lock();
-		//cv.wait(lk, [&]{return step == "Server Started";});
+		////lk.lock();
+		////cv.wait(lk, [&]{return step == "Server Started";});
 
-		//int result = tcpc.connect(IPAddressString);
-		//ASSERT_EQ(result, 0);
-		//step = "Client Connecting";
+		////int result = tcpc.connect(IPAddressString);
+		////ASSERT_EQ(result, 0);
+		////step = "Client Connecting";
 
-		//lk.unlock();
-		//cv.notify_one();
+		////lk.unlock();
+		////cv.notify_one();
 
-		//lk.lock();
-		//cv.wait(lk, [&]{return step == "Server Received Connection";});
+		////lk.lock();
+		////cv.wait(lk, [&]{return step == "Server Received Connection";});
 
-		//std::string payload = "This is the data";
-		//tcpc.send(payload.data(), static_cast<unsigned int>(payload.length()));
-		//step = "Client Sending Data";
+		////std::string payload = "This is the data";
+		////tcpc.send(payload.data(), static_cast<unsigned int>(payload.length()));
+		////step = "Client Sending Data";
 
-		//lk.unlock();
-		//cv.notify_one();
+		////lk.unlock();
+		////cv.notify_one();
 
-		//lk.lock();
-		//cv.wait(lk, [&]{return step == "Server Received Data";});
-		//lk.unlock();
-	//}};
+		////lk.lock();
+		////cv.wait(lk, [&]{return step == "Server Received Data";});
+		////lk.unlock();
+	////}};
 	
-	//clientThread.join();
-	//serverThread.join();
-}
+	////clientThread.join();
+	////serverThread.join();
+//}
 

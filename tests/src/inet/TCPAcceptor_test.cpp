6e046fe5013fa41d518e5b5ebdccfc910874fcf6
kevincar <kevincarrolldavis@gmail.com>
2018-08-03 02:04:37 -0600

    Working on getConnections testing

--- tests/src/inet/TCPAcceptor_test.cpp
+++ tests/src/inet/TCPAcceptor_test.cpp
@@ -19,6 +19,23 @@ TEST(TCPAcceptorTest, constructor)
 			});
 }
 
-//TEST(TCPAcceptorTest, getConnections)
-//{
-//}
+TEST(TCPAcceptorTest, getConnections)
+{
+	//Acceptor
+	std::shared_ptr<inet::TCPAcceptor::AcceptHandler> acceptHandler = std::make_shared<inet::TCPAcceptor::AcceptHandler>([](std::shared_ptr<inet::TCPConnection> const& connection)->std::shared_ptr<inet::TCPConnection>{
+			return std::shared_ptr<inet::TCPConnection> {nullptr};
+			});
+
+	// processor
+	std::shared_ptr<inet::MasterConnection::processHandler> connectionHandler = std::make_shared<inet::MasterConnection::processHandler>([](std::shared_ptr<inet::IPConnection>)->bool{
+			return true;
+			});
+
+	inet::TCPAcceptor tcpa (acceptHandler, connectionHandler);
+
+	//std::shared_ptr<std::vector<std::shared_ptr<inet::TCPConnection const>>> pConnections = tcpa.getConnections();
+
+	//size_t nConnections = pConnections->size();
+
+	//ASSERT_EQ(nConnections, static_cast<size_t>(1));
+}

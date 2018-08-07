7e88ead9ea5ad20d2548e973200066bac0c98806
kevincar <kevincarrolldavis@gmail.com>
2018-08-06 20:38:15 -0400

    Rename MasterConnection::ProcessHandler

--- include/inet/MasterConnection.hpp
+++ include/inet/MasterConnection.hpp
@@ -13,15 +13,15 @@ namespace inet
 	class MasterConnection
 	{
 		public:
-			typedef std::function<bool (std::shared_ptr<IPConnection> const&)> processHandler;
+			typedef std::function<bool (IPConnection const&)> ProcessHandler;
 			
 			MasterConnection(void);
 			~MasterConnection(void);
 			bool isListening(void) const;
 			unsigned long getNumConnections(void) const;
-			//unsigned int createMasterTCP(std::shared_ptr<processHandler> const& pAcceptPH, std::shared_ptr<processHandler> const& pChildPH);
+			unsigned int createTCPAcceptor(std::shared_ptr<TCPAcceptor::AcceptHandler> const& pAcceptPH, std::shared_ptr<TCPAcceptor::ProcessHandler> const& pChildPH);
 			void removeMasterTCP(unsigned int connID);
-			unsigned int createUDPConnection(std::shared_ptr<processHandler> const& pPH);
+			unsigned int createUDPConnection(std::shared_ptr<ProcessHandler> const& pPH);
 			void removeUDPConnection(unsigned int connID);
 			void acceptConnection(unsigned int masterID, std::shared_ptr<TCPConnection> const& newTCPConnection);
 			void stopListening(void);
@@ -34,9 +34,9 @@ namespace inet
 			std::vector<TCPAcceptor> acceptors;
 			std::mutex acceptor_mutex;
 
-			std::map<unsigned int, std::shared_ptr<processHandler>> processHandlers;
+			std::map<unsigned int, std::shared_ptr<ProcessHandler>> processHandlers;
 			mutable std::mutex proc_mutex;
-			std::map<unsigned int, std::shared_ptr<processHandler>> masterChildProcessHandlers;
+			std::map<unsigned int, std::shared_ptr<ProcessHandler>> masterChildProcessHandlers;
 			mutable std::mutex mcproc_mutex;
 			bool listening = false;
 			mutable std::mutex listening_mutex;

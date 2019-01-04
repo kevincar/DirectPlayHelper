fe88d429718c2154c7f89133642f1aab5c1c8d3b
kevincar <kevincarrolldavis@gmail.com>
2019-01-04 11:47:28 -0700

    Added echos

--- scripts/win/install-dependencies.bat
+++ scripts/win/install-dependencies.bat
@@ -5,8 +5,14 @@
 :: G3LOGGER
 git clone https://github.com/KjellKod/g3log.git
 cd g3log
+
+echo "Creating a build directory"
 mkdir build
+echo "Moving into build directory for g3log"
 cd build
 
+echo "calling CMAKE for g3log dependency"
 cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 12" ..
+
+echo "Building g3log"
 msbuild g3log.sln /p:Configuration=Release /p:Platform=Win32

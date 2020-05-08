1e661e874642f962b0140f4b454261533bf96398
Kevin Davis <kevincarrolldavis@gmail.com>
2020-05-07 23:59:40 -0400

    Base line for functioning compiling

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
-#include <memory>
-#include <g3log/g3log.hpp>
-#include <g3log/logworker.hpp>
-#include "DPServer.hpp"
-#include "CustomSink.hpp"
+//#include <memory>
+//#include <g3log/g3log.hpp>
+//#include <g3log/logworker.hpp>
+//#include "DPServer.hpp"
+//#include "CustomSink.hpp"
 
 int main(int argc, char const* argv[]) 
 {
 
 	// Initialize our logger
-	std::unique_ptr<g3::LogWorker> logWorker{g3::LogWorker::createLogWorker()};
-	logWorker->addSink(std::make_unique<CustomSink>(), &CustomSink::ReceiveLogMessage);
-	g3::initializeLogging(logWorker.get());
+	//std::unique_ptr<g3::LogWorker> logWorker{g3::LogWorker::createLogWorker()};
+	//logWorker->addSink(std::make_unique<CustomSink>(), &CustomSink::ReceiveLogMessage);
+	//g3::initializeLogging(logWorker.get());
 
 
-	std::unique_ptr<DPServer> app = std::unique_ptr<DPServer>(new DPServer(argc, argv)); 
-	app->start();
+	//std::unique_ptr<DPServer> app = std::unique_ptr<DPServer>(new DPServer(argc, argv)); 
+	//app->start();
+	
+
+	//LOG(DEBUG) << "Hello, World!";
+	std::cout << "Hello, world!" << std::endl;
 
 	return 0;
 }

8b15e7e2707b9baecf3488ba702acef214dffdd1
Kevin Davis <kevincarrolldavis@gmail.com>
2020-05-15 17:48:38 -0400

    Unlock the main function for testing

--- tests/src/main_test.cpp
+++ tests/src/main_test.cpp
@@ -1,17 +1,17 @@
-//#define G3_DYNAMIC_LOGGING
+#define G3_DYNAMIC_LOGGING
 
-//#include <g3log/g3log.hpp>
-//#include <g3log/logworker.hpp>
-//#include "../include/CustomSink.hpp"
+#include <g3log/g3log.hpp>
+#include <g3log/logworker.hpp>
+#include "../include/CustomSink.hpp"
 #include "gtest/gtest.h"
 
 int main(int argc, char* argv[])
 {
 	// Initialize the logger
-	//std::unique_ptr<g3::LogWorker> logWorker{g3::LogWorker::createLogWorker()};
-	//logWorker->addSink(std::make_unique<CustomSink>(), &CustomSink::ReceiveLogMessage);
-	//g3::initializeLogging(logWorker.get());
-	//g3::log_levels::setHighest(WARNING);
+	std::unique_ptr<g3::LogWorker> logWorker{g3::LogWorker::createLogWorker()};
+	logWorker->addSink(std::make_unique<CustomSink>(), &CustomSink::ReceiveLogMessage);
+	g3::initializeLogging(logWorker.get());
+	g3::log_levels::setHighest(WARNING);
 	
 	// Begin testing
 	::testing::InitGoogleTest(&argc, argv);

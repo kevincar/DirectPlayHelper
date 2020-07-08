92ba142728a09893eaf71fd312f1691ed0d1d0df
Kevin Davis <kevincarrolldavis@gmail.com>
2020-07-08 19:17:02 -0400

    Adding more IPConnection functionality

--- include/inet/IPConnection.hpp
+++ include/inet/IPConnection.hpp
@@ -7,6 +7,7 @@
 #include "inet/Socket.hpp"
 #include "inet/ServiceAddress.hpp"
 #include <memory>
+#include <thread>
 
 #ifdef HAVE_SOCKET_H
 #define SOCKLEN socklen_t
@@ -22,33 +23,43 @@ namespace inet
 	class IPConnection
 	{
 		public:
-			IPConnection(int type, int protocol);
-			IPConnection(int captureRawSocket, int type, int protocol, IPConnection const& parentConnection, sockaddr_in& destAddr);
+			typedef std::function<bool(IPConnection const&)> ConnectionHandler;
 
-			virtual ~IPConnection() = default;
+			virtual ~IPConnection();
 
 			std::string const getAddressString(void) const;
 			std::string const getIPAddressString(void) const;
 			std::string const getPortString(void) const;
 			std::string const getPort(void) const;
+			std::string const getDestAddressString(void) const;
 			void setAddress(std::string const& address);
 			void listen(void);
 			bool isDataReady(double timeout) const;
 			int connect(std::string addressString);
 			int send(char const* data, unsigned int const data_len) const;
 			int recv(char* buffer, unsigned int buffer_len) const;
+			bool isDone(void) const;
+			void startHandlerProcess(ConnectionHandler const& connectionHandler);
+			void endHandlerProcess(void);
 
 			operator int const() const;
 
 		protected:
+			IPConnection(int type, int protocol);
+			IPConnection(int captureRawSocket, int type, int protocol, IPConnection const& parentConnection, sockaddr_in& destAddr);
+
+			void updateSrcAddr(void);
+			void configureSocket(void);
+
 			mutable std::mutex socket_mutex;
 			mutable std::mutex srcAddr_mutex;
 			mutable std::mutex destAddr_mutex;
+			mutable std::mutex done_mutex;
 			Socket socket;
 			ServiceAddress srcAddress {};
 			ServiceAddress destAddress {};
-			
-			void updateSrcAddr(void);
+			bool done = false;
+			std::thread handlerProcess;
 	};
 }
 

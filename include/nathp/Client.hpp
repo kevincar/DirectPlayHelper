bb07229a317498b3d9b747d10c68d5821bc93c28
Kevin Davis <kevincarrolldavis@gmail.com>
2020-07-16 11:41:08 -0700

    NATHP Client

--- include/nathp/Client.hpp
+++ include/nathp/Client.hpp
@@ -3,19 +3,27 @@
 #define NATHP_CLIENT_HPP
 
 #include "inet/MasterConnection.hpp"
+#include "nathp/constants.hpp"
+#include "nathp/Packet.hpp"
 
 namespace nathp
 {
 	class Client
 	{
 		public:
-			Client(std::string serverAddress);
+			Client(std::string serverIPAddress, int port = NATHP_PORT, bool start = true);
 			
+			void connect(void);
 			std::vector<unsigned int> getClientList(void) const;
-		private:
-			inet::TCPConnection serverConnection;
 
-			inet::IPConnection::ConnectionHandler connectionHandler;
+			int nConnectionRetries = -1;
+		private:
+			bool connectionHandler(inet::IPConnection const& connection);
+			void processPacket(Packet const packet) const noexcept;
+
+			inet::IPConnection::ConnectionHandler ch;
+			inet::TCPConnection serverConnection;
+			std::string serverAddress;
 	};
 }
 

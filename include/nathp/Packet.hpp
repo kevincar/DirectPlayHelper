7798ede016ff306582b84c599fc98aa584d31a7b
Kevin Davis <kevincarrolldavis@gmail.com>
2020-07-18 21:53:04 -0700

    payload data transfer
    These functions allow effective payload transfer for different types of
    vectors

--- include/nathp/Packet.hpp
+++ include/nathp/Packet.hpp
@@ -24,8 +24,34 @@ namespace nathp
 			unsigned int size(void) const;
 
 			void setData(unsigned char const* data, unsigned int size);
+			template<typename T> void setPayload(std::vector<T>& payload)
+			{
+				this->vector_copy(payload, this->payload);
+				return;
+			}
+			template<typename T> void getPayload(std::vector<T>& dest)
+			{
+				this->vector_copy(this->payload, dest);
+				return;
+			}
+
 		private:
 			mutable unsigned char* packetData = nullptr;
+
+			template<typename T, typename U> static void vector_copy(std::vector<T>& src, std::vector<U>& dest)
+			{
+				double src_type_size = sizeof(T);
+				double dest_type_size = sizeof(U);
+				double ratio = src_type_size / dest_type_size;
+				unsigned int size = (src.size() * ratio);
+				
+				U* start = (U*)src.data();
+				U* end = start + size;
+
+				dest.resize(size);
+				dest.assign(start, end);
+				return;
+			}
 	};
 
 	struct _Packet

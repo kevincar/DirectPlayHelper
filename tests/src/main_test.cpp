80b039bbedd302bef8dbaaa3eb73ea540370d694
Kevin Davis <kevincarrolldavis@gmail.com>
2020-07-18 21:59:25 -0700

    Explicit handle to log

--- tests/src/main_test.cpp
+++ tests/src/main_test.cpp
@@ -9,10 +9,10 @@ int main(int argc, char* argv[])
 {
 	// Initialize the logger
 	std::unique_ptr<g3::LogWorker> logWorker{g3::LogWorker::createLogWorker()};
-	logWorker->addSink(std::make_unique<CustomSink>(), &CustomSink::ReceiveLogMessage);
+	std::unique_ptr<g3::SinkHandle<CustomSink>> handle = logWorker->addSink(std::make_unique<CustomSink>(), &CustomSink::ReceiveLogMessage);
 	g3::initializeLogging(logWorker.get());
 	g3::log_levels::setHighest(DEBUG);
-	
+
 	// Begin testing
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();

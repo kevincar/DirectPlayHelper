b79194ff6ae4870ee02a0ffd92f5d57f9d0cf16d
Kevin Davis <kevincarrolldavis@gmail.com>
2020-07-18 21:42:00 -0700

    Add thread ID to the logging information
    There are many threads working at once. This is needed to track possible
    dead-locks

--- include/CustomSink.hpp
+++ include/CustomSink.hpp
@@ -7,6 +7,21 @@ class CustomSink
 	public:
 	void ReceiveLogMessage(g3::LogMessageMover logEntry) 
 	{
-		std::cout << logEntry.get().toString() << std::endl;
+		std::cout << logEntry.get().toString(&CustomSink::CustomFormat) << std::endl;
+	}
+
+	static std::string CustomFormat(g3::LogMessage const& msg)
+	{
+		std::string out;
+		out.append(msg.timestamp() + "\t"
+				+ msg.level() 
+				+ " ["
+				+ msg.threadID()
+				+ " "
+				+ msg.file() 
+				+ "->" 
+				+ msg.function() 
+				+ ":" + msg.line() + "]\t");
+		return out;
 	}
 };

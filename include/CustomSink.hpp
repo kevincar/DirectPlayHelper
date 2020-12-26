573bd72e474cbe602404700c547e0f57c3ea570c
Kevin Davis <kevincarrolldavis@gmail.com>
2020-12-25 22:45:57 -0700

    Google C++ Styling

--- include/CustomSink.hpp
+++ include/CustomSink.hpp
@@ -1,12 +1,16 @@
-#include <string>
+#ifndef INCLUDE_CUSTOMSINK_HPP_
+#define INCLUDE_CUSTOMSINK_HPP_
+
 #include <iostream>
+#include <string>
+
 #include <g3log/logmessage.hpp>
 
-class CustomSink 
-{
-	public:
-	void ReceiveLogMessage(g3::LogMessageMover logEntry) 
-	{
-		std::cout << logEntry.get().toString() << std::endl;
-	}
+class CustomSink {
+ public:
+  void ReceiveLogMessage(g3::LogMessageMover logEntry) {
+    std::cout << logEntry.get().toString() << std::endl;
+  }
 };
+
+#endif  // INCLUDE_CUSTOMSINK_HPP_

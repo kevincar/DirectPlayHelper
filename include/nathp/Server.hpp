06e70cf26e960f1d18cb5f170fd82287b67238ce
Kevin Davis <kevincarrolldavis@gmail.com>
2020-12-27 23:53:37 -0700

    nathp include google c++ style change

--- include/nathp/Server.hpp
+++ include/nathp/Server.hpp
@@ -1,49 +1,49 @@
 
-#ifndef NATHP_SERVER_HPP
-#define NATHP_SERVER_HPP
+#ifndef INCLUDE_NATHP_SERVER_HPP_
+#define INCLUDE_NATHP_SERVER_HPP_
+
+#include <functional>
+#include <memory>
+#include <vector>
 
 #include "inet/MasterConnection.hpp"
-#include "nathp/constants.hpp"
-#include "nathp/Packet.hpp"
 #include "nathp/ClientRecord.hpp"
+#include "nathp/Packet.hpp"
+#include "nathp/constants.hpp"
 
-namespace nathp
-{
-	class Server
-	{
-		public:
-			typedef std::function<bool(char const*, unsigned int)> ProcessHandler;
+namespace nathp {
+class Server {
+ public:
+  // A ProcessHandler is a function callback to process data coming into the
+  // server
+  using std::function<bool(char const*, unsigned int)> = ProcessHandler;
 
-			enum State : unsigned char
-			{
-				OFF,
-				STARTING,
-				READY,
-				BUSY
-			};
+  enum State : unsigned char { OFF, STARTING, READY, BUSY };
 
-			Server(inet::TCPAcceptor::AcceptHandler const& acceptHandler, ProcessHandler const& processHandler, unsigned int port = NATHP_PORT);
+  Server(inet::TCPAcceptor::AcceptHandler const& accept_handler,
+         ProcessHandler const& process_handler, unsigned int port = NATHP_PORT);
 
-			std::vector<ClientRecord> getClientList(void) const;
-			bool connectoToClient(unsigned int clientId);
-			State getState(void) const;
+  std::vector<ClientRecord> getClientList(void) const;
+  bool connectoToClient(unsigned int clientId);
+  State getState(void) const;
 
-		private:
-			bool internalAcceptHandler(inet::TCPConnection const& conn);
-			bool internalProcessHandler(inet::TCPConnection const& conn);
-			void processMessage(inet::TCPConnection const& connection, Packet const& packet);
-			void setState(State s);
+ private:
+  bool internalAcceptHandler(inet::TCPConnection const& conn);
+  bool internalProcessHandler(inet::TCPConnection const& conn);
+  void processMessage(inet::TCPConnection const& connection,
+                      Packet const& packet);
+  void setState(State s);
 
-			std::unique_ptr<inet::MasterConnection> pMasterConnection;
+  std::unique_ptr<inet::MasterConnection> pMasterConnection;
 
-			inet::TCPAcceptor::AcceptHandler externalAcceptHandler;
-			ProcessHandler externalProcessHandler;
+  inet::TCPAcceptor::AcceptHandler externalAcceptHandler;
+  ProcessHandler externalProcessHandler;
 
-			unsigned int main_port;
-			State state = State::OFF;
+  unsigned int main_port;
+  State state = State::OFF;
 
-			mutable std::mutex state_mutex;
-	};
-}
+  mutable std::mutex state_mutex;
+};
+}  // namespace nathp
 
-#endif /* NATHP_SERVER_HPP */
+#endif  // INCLUDE_NATHP_SERVER_HPP_

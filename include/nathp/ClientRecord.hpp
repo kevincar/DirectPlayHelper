6cbb83dbd5d008221ae3a7f9ac878a33c74a36b1
Kevin Davis <kevincarrolldavis@gmail.com>
2021-01-01 13:55:23 -0700

    Refactor ClientRecord
    Added private and public addresses
    
    Reformatted the internal structures that form the binary packed
    ClientRecord for transmission in packets over a network
    
    Removed the use of a static pointer in favor of a uint8_t vector that is
    used to temporarilty store data.

--- include/nathp/ClientRecord.hpp
+++ include/nathp/ClientRecord.hpp
@@ -3,33 +3,72 @@
 #define INCLUDE_NATHP_CLIENTRECORD_HPP_
 
 #include <string>
+#include <vector>
 
 namespace nathp {
 #pragma pack(push, 1)
+struct _address {
+  uint8_t _1;
+  uint8_t _2;
+  uint8_t _3;
+  uint8_t _4;
+  uint16_t port;
+};
+#pragma pack(pop)
+#pragma pack(push, 1)
 struct _ClientRecord {
   unsigned int id;
-  uint8_t addressLen;
-  unsigned char address[];
+  unsigned char state;
+  _address private_address;
+  _address public_address;
 };
 #pragma pack(pop)
 
 class ClientRecord {
  public:
-  unsigned int id;
-  std::string address;
+  enum State : unsigned char {connecting, connected, punching, idle};
+  ClientRecord(unsigned int id);
+  ClientRecord(std::vector<uint8_t> const& data);
+  ClientRecord(unsigned char const* data);
 
   unsigned char const* data(void) const noexcept;
   unsigned int size(void) const noexcept;
-  void setData(unsigned char const* data, unsigned int const size) noexcept;
+  void assign(unsigned char const* data) noexcept;
+  template<typename T>
+  void assign(T const& start, T const& end);
+
+  template<typename T>
+  ClientRecord& operator=(T const& data);
+
+  // The ID is assigned the same integer as the file descriptor for the
+  // connection on the server
+  unsigned int id;  
+  State state = State::connecting;
+  std::string private_address;
+  std::string public_address;
 
  private:
-  mutable _ClientRecord* _clientRecord = nullptr;
+  void fromData(_ClientRecord const& client_record_data);
+  _ClientRecord&& toData(void) const;
+  std::string addressFromData(_address const& address_data) const;
+  _address const addressToData(std::string const& address) const;
 
-  void resetClientRecord(unsigned int const size) const noexcept;
-  void getClientRecord(void) const noexcept;
-  void setClientRecord(unsigned char const* data,
-                       unsigned int const size) noexcept;
+  mutable std::vector<uint8_t> data_;
 };
+
+
+template<typename T>
+void ClientRecord::assign(T const& start, T const& end) {
+  this->data_.assign(start, end);
+  _ClientRecord const* cr = reinterpret_cast<_ClientRecord const*>(&(*this->data_.begin()));
+  this->fromData(*cr);
+}
+
+template<typename T>
+ClientRecord& ClientRecord::operator=(T const& data) {
+  this->assign(data.begin(), data.end());
+  return *this;
+}
 }  // namespace nathp
 
 #endif  // INCLUDE_NATHP_CLIENTRECORD_HPP_
--- src/nathp/ClientRecord.cpp
+++ src/nathp/ClientRecord.cpp
@@ -2,51 +2,79 @@
 #include "nathp/ClientRecord.hpp"
 
 #include <cstdlib>
+#include <sstream>
+#include <vector>
 
 namespace nathp {
+ClientRecord::ClientRecord(unsigned int id) : id(id) {}
+
+ClientRecord::ClientRecord(std::vector<uint8_t> const& data) {
+  _ClientRecord const* cr = 
+      reinterpret_cast<_ClientRecord const*>(&(*data.begin()));
+  this->fromData(*cr);
+}
+
+ClientRecord::ClientRecord(unsigned char const* data) {
+  _ClientRecord const* cr = reinterpret_cast<_ClientRecord const*>(data);
+  this->fromData(*cr);
+}
+
 unsigned char const* ClientRecord::data(void) const noexcept {
-  this->getClientRecord();
-  return reinterpret_cast<unsigned char const*>(this->_clientRecord);
+  _ClientRecord const cr = this->toData();
+  unsigned int const kSize = sizeof(cr);
+  uint8_t const* start = reinterpret_cast<uint8_t const*>(&cr);
+  uint8_t const* end = start + kSize;
+  this->data_.assign(start, end);
+  return &(*this->data_.begin());
 }
 
 unsigned int ClientRecord::size(void) const noexcept {
-  return sizeof(_ClientRecord) + this->address.size();
+  return sizeof(_ClientRecord);
 }
 
-void ClientRecord::setData(unsigned char const* data,
-                           unsigned int const size) noexcept {
-  this->setClientRecord(data, size);
-  this->id = this->_clientRecord->id;
-  unsigned char* begin = this->_clientRecord->address;
-  unsigned char* end = begin + this->_clientRecord->addressLen;
-  this->address.assign(begin, end);
-  return;
+void ClientRecord::assign(unsigned char const* data) noexcept {
+  _ClientRecord const* cr = reinterpret_cast<_ClientRecord const*>(data);
+  this->fromData(*cr);
 }
 
-void ClientRecord::resetClientRecord(unsigned int const size) const noexcept {
-  if (this->_clientRecord != nullptr) {
-    delete this->_clientRecord;
-  }
-  this->_clientRecord =
-      reinterpret_cast<_ClientRecord*>(new unsigned char[size]);
-  return;
+void ClientRecord::fromData(_ClientRecord const& client_record_data) {
+  this->id = client_record_data.id;
+  this->state = static_cast<ClientRecord::State>(client_record_data.state);
+  this->private_address =
+    this->addressFromData(client_record_data.private_address);
+  this->public_address =
+    this->addressFromData(client_record_data.public_address);
 }
 
-void ClientRecord::getClientRecord(void) const noexcept {
-  this->resetClientRecord(this->size());
-  this->_clientRecord->id = this->id;
-  this->_clientRecord->addressLen = static_cast<uint8_t>(this->address.size());
-  char const* begin = this->address.data();
-  char const* end = begin + this->address.size();
-  std::copy(begin, end, this->_clientRecord->address);
-  return;
+_ClientRecord&& ClientRecord::toData(void) const {
+  _ClientRecord client_record;
+  client_record.id = this->id;
+  client_record.state = this->state;
+  client_record.private_address = this->addressToData(this->private_address);
+  client_record.public_address = this->addressToData(this->public_address);
+  return std::move(client_record);
 }
 
-void ClientRecord::setClientRecord(unsigned char const* data,
-                                   unsigned int const size) noexcept {
-  this->resetClientRecord(size);
-  unsigned char const* begin = data;
-  unsigned char const* end = data + size;
-  std::copy(begin, end, reinterpret_cast<unsigned char*>(this->_clientRecord));
+std::string ClientRecord::addressFromData(_address const& address_data) const {
+  return std::to_string(address_data._1) + "." +
+    std::to_string(address_data._2) + "." + 
+    std::to_string(address_data._3) + "." + 
+    std::to_string(address_data._4) + ":" +
+    std::to_string(address_data.port);
+}
+
+_address const ClientRecord::addressToData(std::string const& address) const {
+  int vals[5] = {};
+  char p;
+  std::stringstream ss {address};
+  ss >> vals[0] >> p >> vals[1] >> p >> vals[2] >> p >> vals[3] >> p >> vals[4];
+  _address a = {
+    static_cast<uint8_t>(vals[0]), 
+    static_cast<uint8_t>(vals[1]),
+    static_cast<uint8_t>(vals[2]), 
+    static_cast<uint8_t>(vals[3]),
+    static_cast<uint16_t>(vals[4])
+  };
+  return a;
 }
 }  // namespace nathp

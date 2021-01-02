21ddce65b5615f57c2082304221dbd0d198a3d65
Kevin Davis <kevincarrolldavis@gmail.com>
2021-01-01 23:54:01 -0700

    NATHP Server refractoring
    This refractoring inlcudes variable name changes, and proceses with the
    new packet methods.
    
    It also addresses #10 by adding a client record to the list when a new
    connection is accepted.

--- include/nathp/Server.hpp
+++ include/nathp/Server.hpp
@@ -21,27 +21,31 @@ class Server {
   enum State : unsigned char { OFF, STARTING, READY, BUSY };
 
   Server(inet::TCPAcceptor::AcceptHandler const&& accept_handler,
-         ProcessHandler const&& process_handler, unsigned int port = NATHP_PORT);
+         ProcessHandler const&& process_handler,
+         unsigned int port = NATHP_PORT);
 
-  std::vector<ClientRecord> getClientList(void) const;
-  bool connectoToClient(unsigned int clientId);
-  State getState(void) const;
+  // std::vector<ClientRecord> getClientList(void) const;
+  // bool connectoToClient(unsigned int clientId);
+  // State getState(void) const;
+  int sendPacketTo(Packet const& packet, inet::IPConnection const& conn) const;
 
  private:
   bool internalAcceptHandler(inet::TCPConnection const& conn);
   bool internalProcessHandler(inet::TCPConnection const& conn);
   void processMessage(inet::TCPConnection const& connection,
                       Packet const& packet);
+  void processGetClientId(inet::TCPConnection const& connection,
+                          Packet const& packet) const;
+  void processGetPublicAddress(inet::TCPConnection const& connection,
+                               Packet const& packet) const;
   void setState(State s);
 
-  std::unique_ptr<inet::MasterConnection> pMasterConnection;
-
-  inet::TCPAcceptor::AcceptHandler externalAcceptHandler;
-  ProcessHandler externalProcessHandler;
-
   unsigned int main_port;
+  inet::TCPAcceptor::AcceptHandler external_accept_handle;
+  ProcessHandler external_process_handle;
+  std::unique_ptr<inet::MasterConnection> p_master_connection;
+  std::vector<ClientRecord> client_list;
   State state = State::OFF;
-
   mutable std::mutex state_mutex;
 };
 }  // namespace nathp
--- src/nathp/Server.cpp
+++ src/nathp/Server.cpp
@@ -7,19 +7,20 @@
 namespace nathp {
 Server::Server(inet::TCPAcceptor::AcceptHandler const&& acceptHandler,
                ProcessHandler const&& processHandler, unsigned int port)
-    : externalAcceptHandler(acceptHandler),
-      externalProcessHandler(processHandler),
+    : external_accept_handle(acceptHandler),
+      external_process_handle(processHandler),
       main_port(port) {
   // Initialize the internal master connection that will handle the
   // server data
-  this->pMasterConnection = std::make_unique<inet::MasterConnection>(1);
+  this->p_master_connection = std::make_unique<inet::MasterConnection>(1);
 
   inet::TCPAcceptor::AcceptHandler ah =
       std::bind(&Server::internalAcceptHandler, this, std::placeholders::_1);
   inet::TCPAcceptor::ProcessHandler ph =
       std::bind(&Server::internalProcessHandler, this, std::placeholders::_1);
   this->setState(Server::State::STARTING);
-  inet::TCPAcceptor* tcpa = this->pMasterConnection->createTCPAcceptor(ah, ph);
+  inet::TCPAcceptor* tcpa =
+      this->p_master_connection->createTCPAcceptor(ah, ph);
 
   // Set the address
   std::string address =
@@ -29,41 +30,54 @@ Server::Server(inet::TCPAcceptor::AcceptHandler const&& acceptHandler,
   this->setState(Server::State::READY);
 }
 
-std::vector<ClientRecord> Server::getClientList(void) const {
-  // LOG(DEBUG) << "Server::getClientList";
-  std::vector<nathp::ClientRecord> result{};
-  std::vector<inet::TCPAcceptor const*> acceptors =
-      this->pMasterConnection->getAcceptors();
-  std::vector<inet::TCPConnection const*> connections =
-      acceptors.at(0)->getConnections();
-  for (inet::TCPConnection const* curConn : connections) {
-    unsigned int connId = static_cast<int>(*curConn);
-    ClientRecord cr;
-    cr.id = connId;
-    cr.address = curConn->getDestAddressString();
-    result.push_back(cr);
-  }
-  // LOG(DEBUG) << "Server: N Connections = " << result.size();
-  return result;
-}
+// std::vector<ClientRecord> Server::getClientList(void) const {
+//// LOG(DEBUG) << "Server::getClientList";
+// std::vector<nathp::ClientRecord> result{};
+// std::vector<inet::TCPAcceptor const*> acceptors =
+// this->p_master_connection->getAcceptors();
+// std::vector<inet::TCPConnection const*> connections =
+// acceptors.at(0)->getConnections();
+// for (inet::TCPConnection const* curConn : connections) {
+// unsigned int connId = static_cast<int>(*curConn);
+// ClientRecord cr{connId};
+// cr.address = curConn->getDestAddressString();
+// result.push_back(cr);
+//}
+//// LOG(DEBUG) << "Server: N Connections = " << result.size();
+// return result;
+//}
 
-bool Server::connectoToClient(unsigned int clientId) { return false; }
+// bool Server::connectoToClient(unsigned int clientId) { return false; }
 
-Server::State Server::getState(void) const {
-  std::lock_guard<std::mutex> state_lock{this->state_mutex};
-  return this->state;
+// Server::State Server::getState(void) const {
+// std::lock_guard<std::mutex> state_lock{this->state_mutex};
+// return this->state;
+//}
+
+int Server::sendPacketTo(Packet const& packet,
+                         inet::IPConnection const& conn) const {
+  char const* data = reinterpret_cast<char const*>(packet.data());
+  unsigned int const kSize = packet.size();
+  return conn.send(data, kSize);
 }
 
 bool Server::internalAcceptHandler(inet::TCPConnection const& conn) {
-  return this->externalAcceptHandler(conn);
+  bool accepted = this->external_accept_handle(conn);
+  if (!accepted) return accepted;
+
+  LOG(DEBUG) << "New client connection";
+  ClientRecord client_record(conn);
+  client_record.public_address = conn.getDestAddressString();
+  this->client_list.push_back(client_record);
+
+  return accepted;
 }
 
 bool Server::internalProcessHandler(inet::TCPConnection const& conn) {
-  // LOG(DEBUG) << "Server: Received Data";
-  unsigned int const buffer_size = 1024 * 4;
-  char buffer[buffer_size]{};
-  int bytes_received = conn.recv(buffer, buffer_size);
-  // LOG(DEBUG) << "Server: Received Data!!!";
+  unsigned int const kBufSize = 1024 * 4;
+  std::vector<uint8_t> buffer(kBufSize, '\0');
+  int bytes_received =
+      conn.recv(reinterpret_cast<char*>(buffer.data()), buffer.size());
   if (bytes_received == -1) {
     LOG(WARNING) << "Failed to receive bytes!";
     return true;
@@ -72,8 +86,8 @@ bool Server::internalProcessHandler(inet::TCPConnection const& conn) {
     return false;
   }
 
-  nathp::Packet packet{};
-  packet.setData((unsigned char*)buffer, bytes_received);
+  nathp::Packet packet{buffer};
+  LOG(DEBUG) << "Received packet of " << packet.size() << " bytes";
   this->processMessage(conn, packet);
   return true;
 }
@@ -81,55 +95,63 @@ bool Server::internalProcessHandler(inet::TCPConnection const& conn) {
 void Server::processMessage(inet::TCPConnection const& connection,
                             Packet const& packet) {
   switch (packet.msg) {
-    case Packet::Message::initClientID: {
-      unsigned int id = static_cast<int>(connection);
-      Packet returnPacket;
-      returnPacket.senderID = 0;
-      returnPacket.recipientID = id;
-      returnPacket.type = Packet::Type::response;
-      returnPacket.msg = packet.msg;
-      returnPacket.setPayload(id);
-      int result = returnPacket.sendVia(connection);
-      if (result < 0) {
-        LOG(WARNING) << "Failed to send initClientID response";
-      }
+    case Packet::Message::getClientId: {
+      this->processGetClientId(connection, packet);
     } break;
-    case Packet::Message::initPublicAddress: {
-      std::string address = connection.getDestAddressString();
-      std::vector<char> vAddress;
-      vAddress.assign(address.begin(), address.end());
-      Packet returnPacket;
-      returnPacket.senderID = 0;
-      returnPacket.recipientID = static_cast<int>(connection);
-      returnPacket.type = Packet::Type::response;
-      returnPacket.msg = packet.msg;
-      returnPacket.setPayload(vAddress);
-      int result = returnPacket.sendVia(connection);
-      if (result < 0) {
-        LOG(WARNING) << "Failed ot send initPublicAddress response";
-      }
+    case Packet::Message::getPublicAddress: {
+      this->processGetPublicAddress(connection, packet);
     } break;
-    case Packet::Message::getClientList: {
-      Packet returnPacket{};
-      returnPacket.type = Packet::Type::response;
-      std::vector<ClientRecord> clientList = this->getClientList();
-      for (ClientRecord cr : clientList) {
-        unsigned char const* begin = cr.data();
-        unsigned char const* end = begin + cr.size();
-        returnPacket.payload.insert(returnPacket.payload.end(), begin, end);
-      }
-      // LOG(DEBUG) << "size of client list: " << clientList.size();
-      // LOG(DEBUG) << "Size of data being stored: " << returnPacket.size();
-      int result = returnPacket.sendVia(connection);
-      if (result < 0) {
-        LOG(WARNING) << "Failed to send getClientList resopnse";
-      }
-    } break;
+      // case Packet::Message::getClientList: {
+      // Packet returnPacket{};
+      // returnPacket.type = Packet::Type::response;
+      // std::vector<ClientRecord> clientList = this->getClientList();
+      // for (ClientRecord cr : clientList) {
+      // unsigned char const* begin = cr.data();
+      // unsigned char const* end = begin + cr.size();
+      // returnPacket.payload.insert(returnPacket.payload.end(), begin, end);
+      //}
+      //// LOG(DEBUG) << "size of client list: " << clientList.size();
+      //// LOG(DEBUG) << "Size of data being stored: " << returnPacket.size();
+      // int result = this->sendPacketTo(returnPacket, connection);
+      // if (result < 0) {
+      // LOG(WARNING) << "Failed to send getClientList resopnse";
+      //}
+    //} break;
     default:
       LOG(WARNING)
           << "NATHP Server - Unrecognized NATHP packet command request";
   }
   return;
+}
+
+void Server::processGetClientId(inet::TCPConnection const& connection,
+                                Packet const& packet) const {
+  unsigned int id = static_cast<int>(connection);
+  Packet returnPacket;
+  returnPacket.sender_id = 0;
+  returnPacket.recipient_id = id;
+  returnPacket.type = Packet::Type::response;
+  returnPacket.msg = packet.msg;
+  returnPacket.setPayload(id);
+  int result = this->sendPacketTo(returnPacket, connection);
+  if (result < 0) {
+    LOG(WARNING) << "Failed to send initClientID response";
+  }
+}
+
+void Server::processGetPublicAddress(inet::TCPConnection const& connection,
+                                     Packet const& packet) const {
+  std::string public_address = connection.getDestAddressString();
+  Packet returnPacket;
+  returnPacket.sender_id = 0;
+  returnPacket.recipient_id = connection;
+  returnPacket.type = Packet::Type::response;
+  returnPacket.msg = packet.msg;
+  returnPacket.setPayload(public_address);
+  int result = this->sendPacketTo(returnPacket, connection);
+  if (result < 0) {
+    LOG(WARNING) << "Failed ot send initPublicAddress response";
+  }
 }
 
 void Server::setState(Server::State s) {

a0384b0ac76a64c091edc7ff7ca3e2123439a311
Kevin Davis <kevincarrolldavis@gmail.com>
2021-01-05 09:33:08 -0500

    Add helper functions with the lock_pack

--- tests/include/nathp/assets/protocol.hpp
+++ tests/include/nathp/assets/protocol.hpp
@@ -13,6 +13,21 @@ class lock_pack {
   std::mutex* status_mutex;
   std::condition_variable* status_cv;
 };
+
+void waitForStatus(std::shared_ptr<lock_pack> p_lock_pack, std::string const& status) {
+  std::unique_lock<std::mutex> status_lock{*p_lock_pack->status_mutex};
+  p_lock_pack->status_cv->wait(
+      status_lock, [&] { return *p_lock_pack->status == status; });
+  status_lock.unlock();
+}
+
+void setStatus(std::shared_ptr<lock_pack> p_lock_pack, std::string const& status) {
+  { std::lock_guard<std::mutex> status_lock{*p_lock_pack->status_mutex};
+    *p_lock_pack->status = status;
+  }
+  p_lock_pack->status_cv->notify_all();
+}
+
 }  // namespace asset
 }  // namespace nathp
 

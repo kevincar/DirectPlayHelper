4251bcd1f739003b52cc3635e5b60716c32fae13
Kevin Davis <kevincarrolldavis@gmail.com>
2021-03-08 21:58:15 -0500

    Comments

--- include/nathp/messages.hpp
+++ include/nathp/messages.hpp
@@ -5,6 +5,12 @@
 
 namespace nathp {
 enum MSG : uint8_t { HEADER };
+
+/*
+ * MSG_HEADER
+ * All data sent over NATHP protocol will have this header at the beginning of
+ * the data
+ */
 #pragma pack(push, 1)
 typedef struct _MSG_HEADER {
   uint16_t size;

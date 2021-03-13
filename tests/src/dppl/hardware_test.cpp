acaf7bd6fef234edc37d3a079877d4476eaa00f5
Kevin Davis <kevincarrolldavis@gmail.com>
2021-03-13 12:21:01 -0500

    For customizing the tests that are ran by using ENV

--- tests/src/dppl/hardware_test.cpp
+++ tests/src/dppl/hardware_test.cpp
@@ -2,8 +2,12 @@
 
 #include <string>
 
-bool hardware_test_check(void) {
-  char* env = std::getenv("HARDWARE_TEST");
+bool test_check(char const* env_var) {
+  char* env = std::getenv(env_var);
   return env != nullptr;
+}
+
+bool hardware_test_check(void) {
+  return test_check("TEST_HARDWARE");
 }
 

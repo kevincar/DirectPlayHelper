aeb7f5abefcc50d668a3192ca47670b6da00c96e
Kevin Davis <kevincarrolldavis@gmail.com>
2021-03-15 23:44:56 -0400

    Gut DirectPlayServer to use the sniffer

--- include/dppl/DirectPlayServer.hpp
+++ include/dppl/DirectPlayServer.hpp
@@ -3,25 +3,26 @@
 #include <experimental/net>
 #include <functional>
 #include <vector>
+
+#include "dppl/PacketSniffer.hpp"
+
 namespace dppl {
 class DirectPlayServer {
  public:
   DirectPlayServer(std::experimental::net::io_context* io_context,
                    std::function<void(std::vector<char>)> forward);
 
-  void stop();
  private:
-  void receive();
-  void receive_handler(std::error_code const& ec,
-                       std::size_t bytes_transmitted);
+  bool receive_handler(std::vector<char> data);
 
   static int const kPort_ = 47624;
   static int const kBufSize_ = 512;
   std::vector<char> buf_;
   std::function<void(std::vector<char>)> forward_;
   std::experimental::net::io_context* io_context_;
-  std::experimental::net::ip::udp::endpoint app_endpoint_;
-  std::experimental::net::ip::udp::socket dpsrvr_socket_;
+  // std::experimental::net::ip::udp::endpoint app_endpoint_;
+  // std::experimental::net::ip::udp::socket dpsrvr_socket_;
+  PacketSniffer sniffer_;
 };
 }  // namespace dppl
 #endif  // INCLUDE_DPPL_DIRECTPLAYSERVER_HPP_
--- src/dppl/DirectPlayServer.cpp
+++ src/dppl/DirectPlayServer.cpp
@@ -10,49 +10,20 @@ DirectPlayServer::DirectPlayServer(
     std::function<void(std::vector<char>)> forward)
     : io_context_(io_context),
       forward_(forward),
-      dpsrvr_socket_(
-          *io_context,
-          std::experimental::net::ip::udp::endpoint(
-              std::experimental::net::ip::address_v4({127, 0, 0, 1}), kPort_)) {
-  this->dpsrvr_socket_.set_option(
-      std::experimental::net::socket_base::broadcast(true));
-  this->dpsrvr_socket_.set_option(
-      std::experimental::net::socket_base::reuse_address(true));
-  this->receive();
+      sniffer_(io_context, std::bind(&DirectPlayServer::receive_handler, this,
+                                     std::placeholders::_1)) {
+  // this->dpsrvr_socket_.set_option(
+  // std::experimental::net::socket_base::broadcast(true));
+  // this->dpsrvr_socket_.set_option(
+  // std::experimental::net::socket_base::reuse_address(true));
 }
 
-void DirectPlayServer::stop() {
-  this->dpsrvr_socket_.cancel();
-  std::experimental::net::defer(*this->io_context_,
-                                [&]() { this->io_context_->stop(); });
-}
-
-void DirectPlayServer::receive() {
-  this->buf_.clear();
-  this->buf_.resize(kBufSize_, '\0');
-  auto handler = std::bind(&DirectPlayServer::receive_handler, this,
-                           std::placeholders::_1, std::placeholders::_2);
-  this->dpsrvr_socket_.async_receive_from(
-      std::experimental::net::buffer(this->buf_), this->app_endpoint_, handler);
-}
-
-void DirectPlayServer::receive_handler(std::error_code const& ec,
-                                       std::size_t bytes_transmitted) {
-  if (!ec) {
-    LOG(DEBUG)
-        << "Direct Play Server received a request... Forwarding the message";
-    DPMessage packet(&this->buf_);
-    this->buf_.resize(packet.header()->cbSize);
-    this->forward_(this->buf_);
-  } else {
-    if (ec == std::experimental::net::error::operation_aborted) {
-      LOG(DEBUG) << "DirectPlayServer sequence aborted. Error: "
-                 << ec.message();
-      return;
-    } else {
-      LOG(WARNING) << "Direct Play Server Receive Error: " << ec.message();
-    }
-  }
-  this->receive();
+bool DirectPlayServer::receive_handler(std::vector<char> data) {
+  this->buf_ = data;
+  LOG(DEBUG)
+      << "Direct Play Server received a request... Forwarding the message";
+  DPMessage packet(&this->buf_);
+  this->forward_(this->buf_);
+  return true;
 }
 }  // namespace dppl

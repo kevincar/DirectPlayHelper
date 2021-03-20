20bf9b592d37e8883849306235d274d703c43d06
Kevin Davis <kevincarrolldavis@gmail.com>
2021-03-20 11:04:57 -0400

    DPLAYI_SUPERPACKEDPLAYER now has bit field structure

--- include/dppl/dplay.h
+++ include/dppl/dplay.h
@@ -188,17 +188,17 @@ typedef struct {
   DWORD ID;                //  MUST contain the player ID of the
                            //  player that is described in this
                            //  structure.
-  DWORD dwPlayerInfoMask;  //  A bit field that indicates which
+  struct {  //  A bit field that indicates which
                            //  optional fields are present. The
                            //  PlayerInfoMask field MUST be a bitmask
                            //  that is composed of the following fields:
-                           //    SN (1 bit): MUST be set if the
+    bool shortNamePresent : 1;       //    SN (1 bit): MUST be set if the
                            //    ShortName field is present in the
                            //    structure.
-                           //    LN (1 bit): MUST be set if the
+    bool longNamePresent : 1;        //    LN (1 bit): MUST be set if the
                            //    LongName field is present in the
                            //    structure.
-                           //    SL (2 bits): MUST be set if the
+    char serviceProviderLength : 2;        //    SL (2 bits): MUST be set if the
                            //    ServiceProviderDataLength field is
                            //    present in the structure. SL MUST be
                            //    set to one of the following values.
@@ -211,7 +211,7 @@ typedef struct {
                            //      0x03 = If the
                            //      ServiceProviderDataLength field
                            //      occupies 4 bytes.
-                           //    PD (2 bits): MUST be set if the
+    char playerDataLength : 2;                       //    PD (2 bits): MUST be set if the
                            //    PlayerDataLength field is present in
                            //    the structure. PD MUST be set to one
                            //    of the following values:
@@ -221,7 +221,7 @@ typedef struct {
                            //      field occupies 2 bytes.
                            //      0x03 = If the PlayerDataLength
                            //      field occupies 4 bytes.
-                           //    PC (2 bits): MUST be set if the
+    char playerCountLength : 2;                       //    PC (2 bits): MUST be set if the
                            //    PlayerCount field is present in the
                            //    structure. PC MUST be set to one of
                            //    the following values:
@@ -231,10 +231,10 @@ typedef struct {
                            //      occupies 2 bytes.
                            //      0x03 = If the PlayerCount field
                            //      occupies 4 bytes.
-                           //    PI (1 bit): MUST be set if the
+    bool parentIDPresent : 1;                       //    PI (1 bit): MUST be set if the
                            //    ParentID field is present in the
                            //    structure.
-                           //    SC (2 bits): MUST be set if the
+    char shortcutCountLength : 2;                       //    SC (2 bits): MUST be set if the
                            //    ShortcutCount field is present in the
                            //    structure. SC MUST be set to one of
                            //    the following values:
@@ -244,8 +244,8 @@ typedef struct {
                            //      occupies 2 bytes.
                            //      0x03 = If the ShortcutCount field
                            //      occupies 4 bytes.
-                           //    X (21 bits): All bits with this label
-                           //    SHOULD be set to zero when sent and
+    int x : 21;            //    X (21 bits): All bits with this label
+  } dwPlayerInfoMask;                       //    SHOULD be set to zero when sent and
                            //    MUST be ignored on receipt.
   DWORD dwSystemPlayerID;  //  If the DPLAYI_PLAYER_SYSPLAYER flag is
                            //  set in the Flags field, this field MUST
@@ -336,6 +336,12 @@ typedef struct {
   //  ShortcutIDCount multiplied by four.I
 } DPLAYI_SUPERPACKEDPLAYER;
 #pragma pack(pop)
+enum class SUPERPACKEDPLAYERFLAGS {
+  issystemplayer = 0x1,
+  isnameserver = 0x2,
+  isingroup = 0x4,
+  islocalplayer = 0x8
+};
 /*
  * DPSECURITYDESC
  *

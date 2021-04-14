4ebbf49495ef7a7e8dd892c42d7db739b18610d8
Kevin Davis <kevincarrolldavis@gmail.com>
2021-04-13 22:51:34 -0400

    Use a resolver instead of an endpoint

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,10 +6,12 @@ class Client {
  public:
   explicit Client(
       std::experimental::net::io_context* io_context,
-      std::experimental::net::ip::tcp::endpoint const& connection_endpoint);
+      std::experimental::net::ip::tcp::resolver::results_type const& endpoints);
 
  private:
-  void connection_handler(std::error_code const& ec);
+  void connection_handler(
+      std::error_code const& ec,
+      std::experimental::net::ip::tcp::endpoint const& endpoint);
 
   std::experimental::net::io_context* io_context_;
   std::experimental::net::ip::tcp::socket connection_;
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,9 +1,27 @@
+#include <g3log/g3log.hpp>
+
 #include "Client.hpp"
 
 namespace dph {
 Client::Client(
     std::experimental::net::io_context* io_context,
-    std::experimental::net::ip::tcp::endpoint const& connection_endpoint)
+    std::experimental::net::ip::tcp::resolver::results_type const& endpoints)
     : io_context_(io_context),
-      connection_(*io_context, std::experimental::net::ip::tcp::v4()) {}
+      connection_(*io_context, std::experimental::net::ip::tcp::v4()) {
+  auto handler = std::bind(&Client::connection_handler, this,
+                           std::placeholders::_1, std::placeholders::_2);
+  std::experimental::net::async_connect(this->connection_, endpoints, handler);
+}
+
+void Client::connection_handler(
+    std::error_code const& ec,
+    std::experimental::net::ip::tcp::endpoint const& endpoint) {
+  if (!ec) {
+    LOG(DEBUG) << "NICE :)";
+  } else {
+    LOG(WARNING)
+        << "Error attempting to connect with the DirectPlayHelper client: "
+        << ec.message();
+  }
+}
 }  // namespace dph

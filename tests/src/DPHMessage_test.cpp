dca17cfabb86409da598d972fcfef95e6cd95e67
Kevin Davis <kevincarrolldavis@gmail.com>
2021-04-24 22:16:59 -0400

    get_message test

--- tests/src/DPHMessage_test.cpp
+++ tests/src/DPHMessage_test.cpp
@@ -10,4 +10,14 @@ TEST(DPHMessageTest, constructor) {
 
   std::vector<char> payload = dph_message.get_payload();
   ASSERT_STREQ(payload.data(), "Hello");
+
+}
+
+TEST(DPHMessageTest, get_message) {
+  std::vector<char> data = {10, 0, 0, 0,   5,   0,   0,   0,   1, 6,
+                            0,  0, 0, 'H', 'e', 'l', 'l', 'o', 0};
+
+  dph::DPHMessage dph_message(data);
+  DPH_MESSAGE* dphm = dph_message.get_message();
+  ASSERT_EQ(dphm->from_id, 10);
 }

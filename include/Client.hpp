ddd0f6d52a0772bae01e31caf2528e9a210b287b
Kevin Davis <kevincarrolldavis@gmail.com>
2021-04-28 22:59:37 -0400

    Add initiale message handling

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,18 +1,35 @@
 #ifndef INCLUDE_CLIENT_HPP_
 #define INCLUDE_CLIENT_HPP_
 #include <experimental/net>
+#include <vector>
+
+#include "DPHMessage.hpp"
+
 namespace dph {
 class Client {
  public:
-  explicit Client(
+  Client(
       std::experimental::net::io_context* io_context,
       std::experimental::net::ip::tcp::resolver::results_type const& endpoints);
 
  private:
+  /* Message Sending */
+  void request_id(void) const;
+  void enumerate_clients(void) const;
+  void forward_message(DPHMessage const& message);
+
+  /* Message Handlers */
+  void request_id_reply_handler(DPHMessage const& message);
+  void enumerate_clients_reply_handler(DPHMessage const& message);
+
+  /* General net handlers */
+  void write_handler(std::error_code const& ec, std::size_t bytes_transmitted);
+
   void connection_handler(
       std::error_code const& ec,
       std::experimental::net::ip::tcp::endpoint const& endpoint);
 
+  std::vector<char> send_buf_;
   std::experimental::net::io_context* io_context_;
   std::experimental::net::ip::tcp::socket connection_;
 };

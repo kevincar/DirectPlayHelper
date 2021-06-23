335ecb0c95358b090c1b93814d7c97e3ec34c6e8
Kevin Davis <kevincarrolldavis@gmail.com>
2021-06-22 23:06:30 -0600

    Start of client record structure

---
+++ include/ClientRecord.hpp
@@ -0,0 +1,24 @@
+#ifndef INCLUDE_CLIENTRECORD_HPP_
+#define INCLUDE_CLIENTRECORD_HPP_
+#include <cstdint>
+#include <experimental/net>
+
+namespace dph {
+#pragma pack(push, 1)
+typedef struct {
+  uint32_t id;
+  uint32_t public_address;
+  uint16_t public_port;
+} CLIENT_RECORD;
+#pragma pack(pop)
+class ClientRecord {
+ public:
+  ClientRecord(uint32_t id, std::experimental::net::ip::tcp::endpoint const&);
+  explicit ClientRecord(std::vector<char> const&);
+  std::vector<char> to_vector();
+ private:
+  uint32_t id_;
+  std::experimental::net::ip::tcp::endpoint public_address_;
+}
+}  // namespace dph
+#endif  // INCLUDE_CLIENTRECORD_HPP_

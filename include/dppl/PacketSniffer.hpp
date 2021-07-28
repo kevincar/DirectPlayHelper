3b9fbd81b9d7b19aa7e0bd3094c3e557e9169b35
Kevin Davis <kevincarrolldavis@gmail.com>
2021-07-28 14:14:22 -0400

    Class Comments

--- include/dppl/PacketSniffer.hpp
+++ include/dppl/PacketSniffer.hpp
@@ -9,8 +9,18 @@
 #include "tins/tins.h"
 
 namespace dppl {
+// A class to capture data packets sent over port 47624 (`kPort_`). DirectPlay
+// protocol uses this port for messages sent by a client to be broadcast over a
+// netowkr in order to search for host servers. Since these messages will never
+// make it past a NAT, the `PacketSniffer` grabs the packets and and forwards
+// them via the `forward_` function to the object that owns the instantiated
+// `PacketSniffer`. Typically this is the `DirectPlayServer`
 class PacketSniffer {
  public:
+  // the `forward_endpoint` is the network socket that the PacketSniffer should
+  // forward the data. Because `PacketSniffer` sniffs on a separate thread,
+  // data is transfered over a network socket in order to avoid race conditions
+  // or using mutex classes
   explicit PacketSniffer(
       std::experimental::net::ip::udp::endpoint const& forward_endpoint);
   ~PacketSniffer();

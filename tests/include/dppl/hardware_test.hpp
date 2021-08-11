26cd3c465d07350bae847ec62a0e3ddb7db903b5
Kevin Davis <kevincarrolldavis@gmail.com>
2021-08-10 20:40:43 -0400

    Add prompt to hardware tests

--- tests/include/dppl/hardware_test.hpp
+++ tests/include/dppl/hardware_test.hpp
@@ -1,5 +1,7 @@
 #ifndef TESTS_INCLUDE_DPPL_HARDWARE_TEST_HPP_
 #define TESTS_INCLUDE_DPPL_HARDWARE_TEST_HPP_
+#include <string>
 bool test_check(char const* env_var);
 bool hardware_test_check(void);
+void prompt(std::string prompt);
 #endif  // TESTS_INCLUDE_DPPL_HARDWARE_TEST_HPP_
--- tests/src/dppl/hardware_test.cpp
+++ tests/src/dppl/hardware_test.cpp
@@ -1,5 +1,6 @@
 #include "dppl/hardware_test.hpp"
 
+#include <iostream>
 #include <string>
 
 bool test_check(char const* env_var) {
@@ -9,5 +10,12 @@ bool test_check(char const* env_var) {
 
 bool hardware_test_check(void) {
   return test_check("TEST_HARDWARE");
+}
+
+void prompt(std::string prompt) {
+  std::cout << prompt;
+  std::string input;
+  std::getline(std::cin, input, '\n');
+  return;
 }
 

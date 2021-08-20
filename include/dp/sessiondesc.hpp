e2dbbc8dbc739a89aa48e86949586e6c98efda71
Kevin Davis <kevincarrolldavis@gmail.com>
2021-08-20 01:14:18 -0400

    Simplify session desc

--- include/dp/sessiondesc.hpp
+++ include/dp/sessiondesc.hpp
@@ -36,80 +36,52 @@ typedef struct {
 } DPSESSIONDESC2;
 #pragma pack(pop)
 
-enum DPSESSIONDESCFLAGS {
-  nonewplayers = 0x1,
-  migratehost = 0x4,
-  shortplayermessage = 0x8,
-  canjoin = 0x20,
-  useping = 0x40,
-  noplayerupdates = 0x80,
-  useauthentication = 0x100,
-  privatesession = 0x200,
-  passwordrequired = 0x400,
-  routeviagamehost = 0x800,
-  getserverplayeronly = 0x1000,
-  usereliableprotocol = 0x2000,
-  preserveorder = 0x4000,
-  optimizeforlatency = 0x8000,
-  acquirevoice = 0x10000,
-  nosessiondescchanges = 0x20000
-};
-
-inline DPSESSIONDESCFLAGS operator|(DPSESSIONDESCFLAGS const &lhs,
-                                    DPSESSIONDESCFLAGS const &rhs) {
-  return static_cast<DPSESSIONDESCFLAGS>(static_cast<int>(lhs) |
-                                         static_cast<int>(rhs));
-}
-
 namespace dp {
 class sessiondesc {
  public:
-  sessiondesc(std::shared_ptr<std::vector<BYTE>> message_data, BYTE *data);
+  enum Flags : int {
+    nonewplayers = 0x1,
+    migratehost = 0x4,
+    shortplayermessage = 0x8,
+    canjoin = 0x20,
+    useping = 0x40,
+    noplayerupdates = 0x80,
+    useauthentication = 0x100,
+    privatesession = 0x200,
+    passwordrequired = 0x400,
+    routeviagamehost = 0x800,
+    getserverplayeronly = 0x1000,
+    usereliableprotocol = 0x2000,
+    preserveorder = 0x4000,
+    optimizeforlatency = 0x8000,
+    acquirevoice = 0x10000,
+    nosessiondescchanges = 0x20000
+  };
 
-  DWORD get_size(void);
-  void set_size(DWORD size);
+  explicit sessiondesc(BYTE* data);
 
-  DPSESSIONDESCFLAGS get_flags(void);
-  void set_flags(DPSESSIONDESCFLAGS flags);
+  std::vector<BYTE> to_vector(void);
 
-  GUID get_guid_instance(void);
-  void set_guid_instance(GUID guid);
-
-  GUID get_guid_application(void);
-  void set_guid_application(GUID guid);
-
-  DWORD get_max_players(void);
-  void set_max_players(DWORD max_players);
-
-  DWORD get_num_current_players(void);
-  void set_num_current_players(DWORD current_players);
-
-  // Skipping session name and password since documention requires that these
-  // be NULL
-
-  DWORD get_session_id(void);
-  void set_session_id(DWORD session_id);
-
-  DWORD get_reserved_2(void);
-  void set_reserved_2(DWORD reserved_value);
-
-  DWORD get_user_1(void);
-  void set_user_1(DWORD user_1_value);
-
-  DWORD get_user_2(void);
-  void set_user_2(DWORD user_2_value);
-
-  DWORD get_user_3(void);
-  void set_user_3(DWORD user_3_value);
-
-  DWORD get_user_4(void);
-  void set_user_4(DWORD user_4_value);
+  Flags flags;
+  GUID instance;
+  GUID application;
+  DWORD max_players;
+  DWORD num_players;
+  DWORD session_id;
+  DWORD reserved2;
+  DWORD user1;
+  DWORD user2;
+  DWORD user3;
+  DWORD user4;
 
  private:
-  std::shared_ptr<std::vector<BYTE>> message_data_;
-  BYTE *data_;
-  DPSESSIONDESC2 *session_;
+  DPSESSIONDESC2* session_;
 };
 }  // namespace dp
+
+inline dp::sessiondesc::Flags operator|(dp::sessiondesc::Flags const& lhs,
+                                        dp::sessiondesc::Flags const& rhs) {
+  return dp::sessiondesc::Flags(static_cast<int>(lhs) | static_cast<int>(rhs));
+}
 
 #endif  // INCLUDE_DP_SESSIONDESC_HPP_

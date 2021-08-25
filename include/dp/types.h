4e3bef6d67ace4f702be563fa8225d50f3911bad
Kevin Davis <kevincarrolldavis@gmail.com>
2021-08-24 23:01:37 -0400

    of course

--- include/dp/types.h
+++ include/dp/types.h
@@ -5,8 +5,8 @@
 
 #include <string>
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-#include <windows.h>
 #include <winsock2.h>
+#include <windows.h>
 typedef unsigned char STR;
 typedef char16_t WSTR;
 typedef uint64_t QWORD;

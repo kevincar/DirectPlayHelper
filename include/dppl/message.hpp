772a0c11623039f9e63edbf1e392cfb331660801
Kevin Davis <kevincarrolldavis@gmail.com>
2021-08-28 15:17:16 -0400

    Simplified message

---
+++ include/dppl/message.hpp
@@ -0,0 +1,31 @@
+#ifndef INCLUDE_DPPL_MESSAGE_HPP_
+#define INCLUDE_DPPL_MESSAGE_HPP_
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+#include "dp/dp.hpp"
+#include "dppl/EndpointIDs.hpp"
+
+namespace dppl {
+
+class proxy;
+// A `dppl::message` is a DirectPlay transmission associated with endpoint
+// metadata; i.e., it's simply the binary information for a transmission that
+// also comes with important information for proxies to know who the message
+// came from and who it's intended for
+class message {
+ public:
+  message(dp::transmission, ENDPOINTIDS, ENDPOINTIDS);
+
+  // Convert the DPProxyMessage into a byte vector
+  std::vector<BYTE> to_vector() const;
+
+  ENDPOINTIDS to;
+  ENDPOINTIDS from;
+  dp::transmission data;
+};
+
+}  // namespace dppl
+#endif  // INCLUDE_DPPL_MESSAGE_HPP_
---
+++ src/dppl/message.cpp
@@ -0,0 +1,21 @@
+#include "dppl/message.hpp"
+
+namespace dppl {
+
+message::message(dp::transmission data, ENDPOINTIDS from, ENDPOINTIDS to)
+    : data(data), from(from), to(to) {}
+
+std::vector<BYTE> message::to_vector() const { 
+  std::vector<BYTE> message_data = this->data.to_vector();
+
+  std::size_t size = sizeof(PROXYMSG) + message_data.size();
+  std::vector<BYTE> result(size, '\0');
+  PROXYMSG* data = reinterpret_cast<PROXYMSG*>(result.data());
+  data->to = this->to;
+  data->from = this->from;
+
+  BYTE* message_data_ptr = reinterpret_cast<BYTE*>(&data->message);
+  std::copy(message_data.begin(), message_data.end(), message_data_ptr);
+  return result;
+}
+}  // namespace dppl

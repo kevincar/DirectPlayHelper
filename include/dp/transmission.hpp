10fb47f10397156ad4dc55c7766c98f3b11d2805
Kevin Davis <kevincarrolldavis@gmail.com>
2021-08-28 15:15:35 -0400

    Fix const inssues

--- include/dp/transmission.hpp
+++ include/dp/transmission.hpp
@@ -10,15 +10,16 @@
 namespace dp {
 class transmission {
  public:
+  transmission(void);
   explicit transmission(std::shared_ptr<std::vector<BYTE>> data);
   explicit transmission(std::vector<BYTE> data);
-  std::vector<BYTE> const& to_vector(void);
-  bool is_dp_message(void);
+  std::vector<BYTE> const& to_vector(void) const;
+  bool is_dp_message(void) const;
 
   std::shared_ptr<message> msg;
 
  private:
-  std::shared_ptr<std::vector<BYTE>> data_;
+  mutable std::shared_ptr<std::vector<BYTE>> data_;
 };
 }  // namespace dp
 #endif  // INCLUDE_DP_TRANSMISSION_HPP_
--- src/dp/transmission.cpp
+++ src/dp/transmission.cpp
@@ -1,6 +1,8 @@
 #include "dp/transmission.hpp"
 
 namespace dp {
+transmission::transmission(void) {}
+
 transmission::transmission(std::shared_ptr<std::vector<BYTE>> data)
     : data_(data) {
   if (this->is_dp_message()) {
@@ -11,14 +13,14 @@ transmission::transmission(std::shared_ptr<std::vector<BYTE>> data)
 transmission::transmission(std::vector<BYTE> data)
     : transmission(std::make_shared<decltype(data)>(data)) {}
 
-std::vector<BYTE> const& transmission::to_vector(void) {
+std::vector<BYTE> const& transmission::to_vector(void) const {
   if (this->is_dp_message()) {
     this->data_ = std::make_shared<std::vector<BYTE>>(this->msg->to_vector());
   }
   return *this->data_.get();
 }
 
-bool transmission::is_dp_message(void) {
+bool transmission::is_dp_message(void) const {
   BYTE* data = this->data_->data();
   BYTE* play = data + sizeof(DPMSG_HEADER) - 0x8;
   return std::string(play, play + 4) == "play";

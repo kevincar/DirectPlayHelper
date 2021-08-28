e9dcab075f7aeacc1eb333649c603b2bc2c34618
Kevin Davis <kevincarrolldavis@gmail.com>
2021-08-28 01:15:37 -0400

    main DP include

---
+++ include/dp/dp.hpp
@@ -0,0 +1,25 @@
+#ifndef INCLUDE_DP_DP_HPP_
+#define INCLUDE_DP_DP_HPP_
+
+#include "dp/types.h"
+#include "dp/sockaddr.hpp"
+#include "dp/header.hpp"
+#include "dp/sessiondesc.hpp"
+#include "dp/securitydesc.hpp"
+#include "dp/packedplayer.hpp"
+#include "dp/playerinfomask.hpp"
+#include "dp/superpackedplayer.hpp"
+#include "dp/enumsessionsreply.hpp"
+#include "dp/enumsessions.hpp"
+#include "dp/requestplayerid.hpp"
+#include "dp/requestgroupid.hpp"
+#include "dp/requestplayerreply.hpp"
+#include "dp/createplayer.hpp"
+#include "dp/enumplayersreply.hpp"
+#include "dp/addforwardrequest.hpp"
+#include "dp/superenumplayersreply.hpp"
+#include "dp/transmission.hpp"
+
+#include "dp/unimplemented.h"
+
+#endif  // INCLUDE_DP_DP_HPP_

fb33151b9ba2f51d0857edce22221c3bd05b1bd1
Kevin Davis <kevincarrolldavis@gmail.com>
2021-08-28 01:15:21 -0400

    dp sizes in dp

--- tests/src/dppl/dplay_size_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "dppl/dplay.h"
-#include "gtest/gtest.h"
-
-TEST(dplay_size, sizes) {
-  ASSERT_EQ(sizeof(BYTE), sizeof(uint8_t));
-  ASSERT_EQ(sizeof(WORD), sizeof(uint16_t));
-  ASSERT_EQ(sizeof(DWORD), sizeof(uint32_t));
-  ASSERT_EQ(sizeof(QWORD), sizeof(uint64_t));
-}
